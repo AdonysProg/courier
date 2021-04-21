e17a86d14d8abaa46d50c8c622f2c964e40ad35f
Adonys <adonysprogramming@gmail.com>
2021-04-21 01:45:47 -0400

    finished

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import pkg from '../package.json';
 import cors from 'cors';
 import morgan from 'morgan';
 import paquetesRoutes from './routes/paquete.routes';
 import remitenteRoutes from './routes/remitente.routes';
 import facturaRoutes from './routes/factura.routes';
 import destinatarioRoutes from './routes/destinatario.routes';
+import authRoutes from './routes/auth.routes';
 import { config } from 'dotenv';
 
 config();
@@ -19,16 +19,6 @@ app.use(
   })
 );
 
-// app.use(cookieParser(process.env.JWT_SECRET_KEY)); Login coming soon
-
-// app.use(
-//   session({
-//     secret: process.env.JWT_SECRET_KEY,
-//     resave: false,
-//     saveUninitialized: false,
-//   })
-// );
-
 app.use(express.json());
 
 app.use(morgan('dev'));
@@ -37,4 +27,5 @@ app.use('/api/paquetes', paquetesRoutes);
 app.use('/api/remitente', remitenteRoutes);
 app.use('/api/factura', facturaRoutes);
 app.use('/api/destinatario', destinatarioRoutes);
+app.use('/api/auth', authRoutes);
 export default app;
